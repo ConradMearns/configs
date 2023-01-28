e0123fd57a2117fed2e6fef1a49c231feadbfa71
Conrad Mearns <conradmearns@pm.me>
2023-01-28 18:46:33 +0000

    Add kmod blacklist

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,6 +3,7 @@
   #imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
   boot.loader.grub.device = "/dev/sda";
   fileSystems."/" = { device = "/dev/sda5"; fsType = "ext4"; };
+  boot.blacklistedKernelModules = [ "rtl8xxxu" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8814au ];
-  boot.kernelModules = [ "8814au" ];
+  # boot.kernelModules = [ "8814au" ];
 }
