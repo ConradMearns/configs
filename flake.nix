0e2daba29119e9aa23f9d46703d4e684649e0ea0
Conrad Mearns <conradmearns@pm.me>
2023-04-03 17:04:57 -0700

    Back to unstable then

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  # inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+  # inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
 
   outputs = { self, nixpkgs, ... }@attrs: {
 
